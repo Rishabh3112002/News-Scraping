09208e6f91d5e41e0dd184eeae3ce61e557e7ecd
Rishabh3112002 <65342857+Rishabh3112002@users.noreply.github.com>
2021-05-14 12:55:45 +0530

    Add files via upload

---
+++ scraping.py
@@ -0,0 +1,27 @@
+from requests_html import HTMLSession
+
+session = HTMLSession()
+url = 'https://news.google.com/topstories?hl=en-IN&gl=IN&ceid=IN:en'
+
+r = session.get(url)
+
+r.html.render(sleep=1, scrolldown=5)
+
+articles = r.html.find('article')
+
+newslist = []
+
+for item in articles:
+    try:
+        newsitem = item.find('h3', first = True)
+        newsarticle = {
+        'title' : newsitem.text,
+        'link' : newsitem.absolute_links
+        }
+        newslist.append(newsarticle)
+    except:
+        pass
+print(len(newslist))
+for i in range (len(newslist)):
+    print(newslist[i])
+
